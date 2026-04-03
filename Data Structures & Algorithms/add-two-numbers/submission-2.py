6e694a9ff890c39d0aec35428c8faa3d5cb9820e
Milton Chung <56741854+MiltonChung@users.noreply.github.com>
2026-04-02 21:28:28 -0700

    Add: add-two-numbers - submission-2

---
+++ Data Structures & Algorithms/add-two-numbers/submission-2.py
@@ -0,0 +1,54 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        carryOne = False
+        curr1 = l1
+        curr2 = l2
+        head = ListNode()
+        resCurr = None
+
+        while curr1 and curr2:
+            nextNum, carryOne = self.carryFn(curr1.val + curr2.val, carryOne)
+            
+            if head.next is None:
+                resCurr = ListNode(nextNum)
+                head.next = resCurr
+            else:
+                newNode = ListNode(nextNum)
+                resCurr.next = newNode
+                resCurr = resCurr.next            
+
+            curr1 = curr1.next
+            curr2 = curr2.next
+
+        while curr1:
+            nextNum, carryOne = self.carryFn(curr1.val, carryOne)
+            resCurr.next = ListNode(nextNum)
+            resCurr = resCurr.next
+            curr1 = curr1.next
+
+        while curr2:
+            nextNum, carryOne = self.carryFn(curr2.val, carryOne)
+            resCurr.next = ListNode(nextNum)
+            resCurr = resCurr.next
+            curr2 = curr2.next
+        
+        if carryOne:
+            resCurr.next = ListNode(1)
+        
+        return head.next
+
+    def carryFn(self, nextNum, carryOne):
+        if carryOne:
+            nextNum += 1
+            carryOne = False
+
+        if nextNum >= 10:
+            nextNum -= 10
+            carryOne = True
+        return [nextNum, carryOne]
