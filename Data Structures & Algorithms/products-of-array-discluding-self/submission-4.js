c8b9589659b21a72a34bc83d520f083d3a00a91a
Milton Chung <56741854+MiltonChung@users.noreply.github.com>
2026-05-07 23:31:21 -0700

    Add: products-of-array-discluding-self - submission-4

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-4.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        const prefix = []
+        const suffix = []
+        const res = []
+
+        for (let i=0; i<nums.length; i++) {
+            if (i === 0) {
+                prefix[i] = 1
+            } else {
+                prefix[i] = prefix[i - 1] * nums[i - 1]
+            }
+        }
+
+        for (let i=nums.length - 1; i>=0; i--) {
+            if (i === nums.length - 1) {
+                suffix[i] = 1
+            } else {
+                suffix[i] = suffix[i + 1] * nums[i + 1]
+            }
+        }
+
+        for (let i=0; i<nums.length; i++) {
+            res[i] = prefix[i] * suffix[i]
+        }
+
+        return res
+    }
+}
